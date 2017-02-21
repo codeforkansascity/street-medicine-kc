f0faaf47f55141839ac154726fd9b741fc7e7020
Phil Huffman <56phil@users.noreply.github.com>
2017-02-21 17:00:20 -0600

    Revert "add bcrypt encryption"

--- admin/js/forms.js
+++ admin/js/forms.js
@@ -1,4 +1,4 @@
-/*
+/* 
  * Copyright (C) 2013 peredur.net
  *
  * This program is free software: you can redistribute it and/or modify
@@ -16,10 +16,10 @@
  */
 
 function formhash(form, password) {
-    // Create a new element input, this will be our hashed password field.
+    // Create a new element input, this will be our hashed password field. 
     var p = document.createElement("input");
 
-    // Add the new element to our form.
+    // Add the new element to our form. 
     form.appendChild(p);
     p.name = "p";
     p.type = "hidden";
@@ -27,10 +27,10 @@ function formhash(form, password) {
     p.value = password.value;
         // alert(p.value);
 
-    // Make sure the plaintext password doesn't get sent.
+    // Make sure the plaintext password doesn't get sent. 
     password.value = "";
 
-    // Finally submit the form.
+    // Finally submit the form. 
     form.submit();
 }
 
@@ -40,15 +40,15 @@ function regformhash(form, uid, email, agency_id, password, conf) {
         alert('You must provide all the requested details. Please try again');
         return false;
     }
-
+    
     // Check the username
-    re = /^\w+$/;
-    if(!re.test(form.username.value)) {
-        alert("Username must contain only letters, numbers and underscores. Please try again");
+    re = /^\w+$/; 
+    if(!re.test(form.username.value)) { 
+        alert("Username must contain only letters, numbers and underscores. Please try again"); 
         form.username.focus();
-        return false;
+        return false; 
     }
-
+    
     // Check that the password is sufficiently long (min 6 chars)
     // The check is duplicated below, but this is included to give more
     // specific guidance to the user
@@ -57,37 +57,37 @@ function regformhash(form, uid, email, agency_id, password, conf) {
         form.password.focus();
         return false;
     }
-
-    // At least one number, one lowercase and one uppercase letter
-    // At least six characters
-    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    
+    // At least one number, one lowercase and one uppercase letter 
+    // At least six characters 
+    var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/; 
     if (!re.test(password.value)) {
         alert('Passwords must contain at least one number, one lowercase and one uppercase letter.  Please try again');
         return false;
     }
-
+    
     // Check password and confirmation are the same
     if (password.value != conf.value) {
         alert('Your password and confirmation do not match. Please try again');
         form.password.focus();
         return false;
     }
-
-    // Create a new element input, this will be our hashed password field.
+        
+    // Create a new element input, this will be our hashed password field. 
     var p = document.createElement("input");
 
-    // Add the new element to our form.
+    // Add the new element to our form. 
     form.appendChild(p);
     p.name = "p";
     p.type = "hidden";
     // p.value = hex_sha512(password.value);
     p.value = password.value;
 
-    // Make sure the plaintext password doesn't get sent.
+    // Make sure the plaintext password doesn't get sent. 
     password.value = "";
     conf.value = "";
 
-    // Finally submit the form.
+    // Finally submit the form. 
     form.submit();
     return true;
 }
